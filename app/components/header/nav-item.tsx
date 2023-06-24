096be574717b83ac07c57c6818fc3f1960af1899
Josue Ribeiro <jr.waydev@gmail.com>
2023-06-24 20:35:01 -0300

    twMerge ClassValue

--- app/components/header/nav-item.tsx
+++ app/components/header/nav-item.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import { usePathname } from 'next/navigation'
 
 type NavItemProps = {
   label: string,
@@ -6,10 +7,15 @@ type NavItemProps = {
 }
 
 export const NavItem = ( { label, href }: NavItemProps ) => {
+
+  const pathname = usePathname();
+
+  const isActived = pathname === href;
+
   return (
     <Link href={href} className="text-gray-400 flex items-center gap-2 font-medium font-mono">
       <span className="text-emerald-400">#</span>
       {label}
     </Link>
   )
-}
+} 
